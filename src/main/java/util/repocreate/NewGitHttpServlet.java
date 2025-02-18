7ec343b527858e00e12435d262abfef33072f34a
rajkumarkvr <rajkumar2005kvr@gmail.com>
2025-02-18 15:20:30 +0530

    test commit

--- src/main/java/util/repocreate/NewGitHttpServlet.java
+++ src/main/java/util/repocreate/NewGitHttpServlet.java
@@ -17,14 +17,7 @@ public class NewGitHttpServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
