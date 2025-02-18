68df4fb9247dbac966635ab68c0de2206bad1b26
rajkumarkvr <rajkumar2005kvr@gmail.com>
2025-02-18 15:23:09 +0530

    test commit

--- src/main/java/util/repocreate/NewGitHttpServlet.java
+++ src/main/java/util/repocreate/NewGitHttpServlet.java
@@ -18,6 +18,12 @@ public class NewGitHttpServlet extends HttpServlet {
     }
 
 	
-
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
