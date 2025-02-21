e1894b21d1a19ea79a9cd8767e3b40a3be777dbd
rajkumarkvr <rajkumar2005kvr@gmail.com>
2025-02-21 10:08:40 +0530

    splitting

--- src/main/java/services/GitAuthService.java
+++ src/main/java/services/GitAuthService.java
@@ -18,11 +18,13 @@ public class GitAuthService extends HttpServlet {
         String repo = request.getParameter("repo");
         String action = request.getParameter("action");
         System.out.println("Username"+user+" reponame"+repo+" action"+action);
-        
+        	repo=repo.replaceAll("/opt/repo/", "").split("/")[1].replaceAll(".git", "");
+        	System.out.println(repo);
         boolean isValid = false;
         int repoId = RepositoryDAO.getInstance().getRepositoryId(repo);
         int userId = UserDAO.getInstance().getUserId(user);
         
+        System.out.println("repoid : "+repoId+" User id : "+userId);
         if(action.equals("fetch")) {
         	if(!RepositoryDAO.getInstance().isPrivate(repoId)) {
         		isValid = true;
@@ -34,6 +36,7 @@ public class GitAuthService extends HttpServlet {
         		isValid = true;
         	}
         }	
+        
         
   
         if(isValid) {
